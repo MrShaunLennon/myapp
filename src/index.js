a9ae4b4e1dfa52d6cb1658b6d38304d3e92a406d
Shaun Lennon <shaunlennon1992@gmail.com>
2020-10-12 16:44:38 +0100

    Bug fixed

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import ReactDOM from 'react-dom';
 
 // Create a react component.
 const App = () => {
-  const buttonText = "Click Me!";
+  const buttonText = { text: 'Click me!' };
+  const labelText = 'Enter name:';
+
   return (
     <div>
-      <label className="label" for="name">
-        Enter Name:
+      <label className="label" htmlFor="name">
+        {labelText}
       </label>
       <input id="name" type="text" />
-      <button style={{backgroundColor: 'blue', color: "white"}}>
-        {buttonText}
+      <button style={{ backgroundColor: 'blue', color: "white" }}>
+        {buttonText.text}
       </button>
     </div>
   );
